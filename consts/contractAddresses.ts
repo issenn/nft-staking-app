b2088baecc5d479ad508ee82ed996941509ac6bd
issenn <28648060+issenn@users.noreply.github.com>
2023-06-10 11:01:33 +0800

    feat: add contract and fix some bug.

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,12 @@
+// export const nftDropContractAddress =
+//   "0x86eAf8D635eb11D5CD801c41744F44e50d4fd8d2";
+// export const tokenContractAddress =
+//   "0x12ea05ba6E8405cBeceB5b51134A821e86858F7E";
+// export const stakingContractAddress =
+//   "0x830Ace54DFbf3a011601bD0Bc1f9806d45D2d0E4";
+export const nftDropContractAddress =
+  "0x4e11aa3c81445a41a61eA74a7600ABc0e7f75633";
+export const tokenContractAddress =
+  "0xa3f44d590AD0C3627BC69a7f2A772B3583a976e3";
+export const stakingContractAddress =
+  "0xA3A3987910527f260376E25b4E4366742237DA60";
